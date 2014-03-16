6d02852077ae86f92d49709c392629074c6c561e
geforcelee <liyunlong880601@gmail.com>
2014-03-16 22:31:36 +0800

    加入手势

---
+++ Assets/Script/FingerEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerEvent : MonoBehaviour {
+
+	void OnSwipe( SwipeGesture gesture ) 
+	{
+		// Total swipe vector (from start to end position)
+		Vector2 move = gesture.Move;
+		
+		// Instant gesture velocity in screen units per second
+		float velocity = gesture.Velocity;
+		
+		// Approximate swipe direction
+		FingerGestures.SwipeDirection direction = gesture.Direction;
+		Debug.Log ("asdfadf   " + direction);
+	}
+}
